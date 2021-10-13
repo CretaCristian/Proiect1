a258027412af5c3b8846eaa3511ae8f90e19ac40
Cristian <emilcristian13@gmail.com>
2021-10-13 10:14:04 +0300

    proiect 2

---
+++ src/App/Classes/Strategie_Mercedez.java
@@ -0,0 +1,20 @@
+package App.Classes;
+
+import App.Interfaces.IStrategie;
+
+public class Strategie_Mercedez implements IStrategie {
+    @Override
+    public int vreme() {
+        return 0;
+    }
+
+    @Override
+    public void downForce() {
+
+    }
+
+    @Override
+    public String pneuri() {
+        return null;
+    }
+}
